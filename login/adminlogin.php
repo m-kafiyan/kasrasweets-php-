f499e2da9144d7ef5c639b395978a2dbba400b76
maedeh kafiyan <maedeh.kafiyan@gmail.com>
2013-10-04 01:16:24 -0700

    admin panel layout underdev

--- login/adminlogin.php
+++ login/adminlogin.php
@@ -6,12 +6,9 @@ $password=$_POST['password'];
  mysql_select_db("kasraproject");
  $sq=mysql_query("select * from adminlogin where username='$user' and pass='$password'");
 	if($row=mysql_fetch_assoc($sq)){
-		?>
-		
-		<? php
+	header("location: adminpage.html");
 	}
 	else{
 			header("location: loginadmin.html");
 		}
 ?>	
-
